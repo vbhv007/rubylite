ff2297cb49dd3d050795cf677eaf099b91b85066
vbhv007 <bhardwajvbhv@gmail.com>
2018-07-10 15:18:49 +0530

    tests added

--- test/rubylite_test.rb
+++ test/rubylite_test.rb
@@ -1,11 +1,32 @@
-require "test_helper"
+require_relative "test_helper"
 
 class RubyliteTest < Minitest::Test
+  def setup
+    @ob = RQuery.new
+    @db = @ob.db_open('vbhv')
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Rubylite::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_rquery_class_works
+    refute_nil @ob
+  end
+
+  def test_db_open_works
+    refute_nil @db
+  end
+
+  def test_first_works
+    assert_equal [["1", "lucifer"]], Users.first
+  end
+
+  def test_last_works
+    assert_equal [['2', 'vaibhav']], Users.last
+  end
+
+  def test_take_works
+    assert_equal [['1', 'lucifer'], ['2', 'vaibhav']], Users.take(2)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "rubylite"
-
+require '../lib/rquery.rb'
 require "minitest/autorun"
